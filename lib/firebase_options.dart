77eeb3d15dd0b7b4aea52e27c050c6b9e47e5a5c
siddiiqui7aslam <165894299+siddiiqui7aslam@users.noreply.github.com>
2024-07-16 15:02:05 +0530

    Delete lib/firebase_options.dart

--- lib/firebase_options.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-
-const FirebaseOptions firebaseOptions = FirebaseOptions(
-  apiKey: "AIzaSyAtZw82wLJBShSjjsh2fEDJ-RiqHwF5SDM",
-  appId: "1:427542442333:android:8322920430afad4ced78b4",
-  messagingSenderId: "427542442333",
-  projectId: "tvs_app",
-);
